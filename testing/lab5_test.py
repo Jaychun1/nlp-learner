36db2ede51df1af16fc26c8de4694c9b7e3ada75
Jaychun1 <Viet.Hoang@noventiq.com>
2025-11-04 15:05:01 +0700

    add_lab4

---
+++ testing/lab5_test.py
@@ -0,0 +1,12 @@
+### Task1
+
+texts = [
+    "This movie is fantastic and I love it!",
+    "I hate this film, it's terrible.",
+    "The acting was superb, a truly great experience.",
+    "What a waste of time, absolutely boring.",
+    "Highly recommend this, a masterpiece.",
+"Could not finish watching, so bad."
+]
+
+labels = [1, 0, 1, 0, 1, 0]
